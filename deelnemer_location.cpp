04d0064bf0c0427c399c4229e443e590a00f94cb
Wim Deprez <vvim@users.noreply.github.com>
2016-08-19 15:26:48 +0200

    First attempt to add array of markers to Google Maps

--- deelnemer_location.cpp
+++ deelnemer_location.cpp
@@ -45,5 +45,33 @@ DeelnemerLocation::~DeelnemerLocation()
 
 void DeelnemerLocation::on_pushButton_showAllDeelnemers_clicked()
 {
+    /* 1) query DB for deelnemers that are official members but don't have their LAT/LNG coordinates yet:
+     *    Google Maps API doesn't allow more than 15 queries in one go => maximum = 15
+     *    QSqlQuery query_no_lat_lng("SELECT * FROM t_deelnemers WHERE lid = 1  AND (lat is NULL OR lng is NULL) LIMIT 15");
+     */
 
+
+    /// bestaande marker is deelnemerMarker
+
+    //                [id, lat, lng, 'title', icon, zIndex],  // icon: see http://stackoverflow.com/questions/7095574/google-maps-api-3-custom-marker-color-for-default-dot-marker/18623391#18623391
+    QString str = "\n\t[%5, %1, %2,   '%3', 'http://maps.google.com/mapfiles/ms/icons/%4-dot.png'],";
+
+    QString markers_js = "";
+
+    // [TODO] markers_js.append(str.arg(latitude).arg(longitude).arg(title).arg(iconcolor).arg(id));
+
+    QFile f(":/html/google_maps_markers.html");
+    if (!f.open(QFile::ReadOnly | QFile::Text))
+    {
+        vvimDebug() << "whoops, cannot read HTML-file";
+        QMessageBox messageBox;
+        messageBox.critical(0,"Error",tr("Kan HTML-bestand niet lezen"));
+        messageBox.setFixedSize(500,200);
+        return;
+    }
+    QTextStream in(&f);
+    QString htmlToLoad = in.readAll();
+    f.close();
+
+    ui->webView->setHtml(htmlToLoad.arg(settings->value("apiKey").toString()).arg(deelnemerMarker->lat).arg(deelnemerMarker->lng).arg(zoom).arg(deelnemerMarker->caption()).arg(markers_js)  );
 }
