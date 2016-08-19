33e9c20c1d3c20917b205849e2360505b34b984f
Wim Deprez <vvim@users.noreply.github.com>
2016-08-19 15:49:04 +0200

    Filling the array with markers
    [Possible expansion] we can use several colors / infowindows to distinguish between the different types of Deelnemer

--- deelnemer_location.cpp
+++ deelnemer_location.cpp
@@ -60,14 +60,44 @@ void DeelnemerLocation::on_pushButton_showAllDeelnemers_clicked()
 
     QString markers_js = "";
 
-    // [TODO] markers_js.append(str.arg(latitude).arg(longitude).arg(title).arg(iconcolor).arg(id));
+    int lidIdx = model_deelnemers->fieldIndex("lid");
+    int latIdx = model_deelnemers->fieldIndex("lat");
+    int lngIdx = model_deelnemers->fieldIndex("lng");
+    int nameIdx = model_deelnemers->fieldIndex("naam");
+    int idIdx = model_deelnemers->fieldIndex("id");
+
+    for ( int i = 0 ; i < model_deelnemers->rowCount() ; ++i )
+    {
+        // [TODO] markers_js.append(str.arg(latitude).arg(longitude).arg(title).arg(iconcolor).arg(id));
+        if( model_deelnemers->index( i, lidIdx ).data().toInt() == 1 )  // NULL toInt() == 0
+        {
+            // we only show markers if the corresponding deelnemer is an official member, so lidIdx should be 1
+            double latitude = model_deelnemers->index( i, latIdx ).data().toDouble();
+            double longitude = model_deelnemers->index( i, lngIdx ).data().toDouble();
+            QString title = model_deelnemers->index( i, nameIdx ).data().toString();
+            int id = model_deelnemers->index( i, idIdx ).data().toInt();
+
+            QString iconcolor = "blue";
+            markers_js.append(str.arg(latitude).arg(longitude).arg(title).arg(iconcolor).arg(id));
+        }
+
+    }
+
+    if(markers_js.length() < 1) // => no deelnemers to be marked
+    {
+        vvimDebug() << "no deelnemers found to be marked, did something go wrong?";
+        return;
+    }
+
+    // chop last ',' from markers_js
+    markers_js.chop(1);
 
     QFile f(":/html/google_maps_markers.html");
     if (!f.open(QFile::ReadOnly | QFile::Text))
     {
-        vvimDebug() << "whoops, cannot read HTML-file";
+        vvimDebug() << "whoops, cannot read HTML-file for the markers";
         QMessageBox messageBox;
-        messageBox.critical(0,"Error",tr("Kan HTML-bestand niet lezen"));
+        messageBox.critical(0,"Error",tr("Kan HTML-bestand met de markers niet lezen"));
         messageBox.setFixedSize(500,200);
         return;
     }
