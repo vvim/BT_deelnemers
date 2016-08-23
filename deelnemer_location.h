afcd85bf5f35278f9f031e8e4e1aaff9f7c9d97f
Wim Deprez <vvim@users.noreply.github.com>
2016-08-23 14:21:38 +0200

    cahnge useragent to "Chrome/1.0" as in stackoverflow.com/questions/27730642/

--- deelnemer_location.h
+++ deelnemer_location.h
@@ -14,8 +14,7 @@ class deelnemer_location;
 class myWebPage : public QWebPage
 {
     virtual QString userAgentForUrl(const QUrl& url) const {
-        // return "Chrome/1.0"; // see https://wiki.qt.io/How_to_set_user_agent_in_Qt_application
-        return "Mozilla/5.0 (X11; Linux i686; rv:40.0) Gecko/20100101 Firefox/40.0";
+        return "Chrome/1.0"; // see https://wiki.qt.io/How_to_set_user_agent_in_Qt_application
     }
 };
 
