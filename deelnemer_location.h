1339653fc4866823f3431878125574db510b8209
Wim Deprez <vvim@users.noreply.github.com>
2016-08-26 16:35:49 +0200

    surpressing SSL errors
    see https://gist.github.com/gokmen/2558692
    
    (trying without changing the user agent)

--- deelnemer_location.h
+++ deelnemer_location.h
@@ -6,6 +6,8 @@
 #include <QSettings>
 #include <QSqlRelationalTableModel>
 #include "sdeelnemermarker.h"
+#include <QNetworkReply>
+#include <QSslConfiguration>
 
 namespace Ui {
 class deelnemer_location;
@@ -34,6 +36,8 @@ private slots:
     void on_pushButton_loadHTML_clicked();
 
     void on_pushButton_getJavaVersion_clicked();
+
+    void sslErrorHandler(QNetworkReply* qnr, const QList<QSslError> & errlist);
 
 private:
     Ui::deelnemer_location *ui;
