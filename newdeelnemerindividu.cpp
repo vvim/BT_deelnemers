c95a8754ab302943514e8be0ca2f4df4ffc266f3
Wim Deprez <vvim@users.noreply.github.com>
2016-11-15 11:42:50 +0100

    Create New Individual: change the background color of widget to sky blue to attract attention
    This way, the user can easy (visually) see the difference between different inputscreens.
    
    Color based on chart located at: http://www.tayloredmktg.com/rgb/

--- newdeelnemerindividu.cpp
+++ newdeelnemerindividu.cpp
@@ -6,6 +6,12 @@ NewDeelnemerIndividu::NewDeelnemerIndividu(QWidget *parent) :
     ui(new Ui::NewDeelnemerIndividu)
 {
     ui->setupUi(this);
+    QPalette Pal(palette());
+
+    // change the background color to sky blue to attract attention http://www.tayloredmktg.com/rgb/
+    Pal.setColor(QPalette::Background, QColor(135,206,250));
+    this->setAutoFillBackground(true);
+    this->setPalette(Pal);
 }
 
 NewDeelnemerIndividu::~NewDeelnemerIndividu()
