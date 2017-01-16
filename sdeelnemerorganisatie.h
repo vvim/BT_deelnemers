585148e261dba782ffe0ed67165a26a8403c62cc
Wim Deprez <vvim@users.noreply.github.com>
2017-01-16 11:46:00 +0100

    SDeelnemerOrganisatie

--- sdeelnemerorganisatie.h
+++ sdeelnemerorganisatie.h
@@ -8,23 +8,35 @@ struct SDeelnemerOrganisatie
 {
     SDeelnemerOrganisatie()
     {
+        contactpersoon_voornaam = "";
+        contactpersoon_familienaam = "";
+        vrijwilligersverzekering = false;
+        doelgroep = "";
+        domein = "";
     }
 
-/*    SDeelnemerOrganisatie(QString _xx)
+    SDeelnemerOrganisatie(QString _contactpersoon_voornaam, QString _contactpersoon_familienaam, bool _vrijwilligersverzekering, QString _doelgroep, QString _domein)
     {
-        xx = _xx;
+        contactpersoon_voornaam = _contactpersoon_voornaam;
+        contactpersoon_familienaam = _contactpersoon_familienaam;
+        vrijwilligersverzekering = _vrijwilligersverzekering;
+        doelgroep = _doelgroep;
+        domein = _domein;
     }
-*/
+
 
     void PrintInformation()
     {
         qDebug() << "... subtype: Organisatie";
-        qDebug() << "..... xx:" /* << xx */ ;
+        qDebug() << "..... contactpersoon voornaam   :" << contactpersoon_voornaam;
+        qDebug() << "..... contactpersoon familienaam:" << contactpersoon_familienaam;
+        qDebug() << "..... vrijwilligersverzekering?:" << vrijwilligersverzekering;
+        qDebug() << "..... doelgroep:" << doelgroep /** << getDoelgroep() **/ ;
+        qDebug() << "..... domein   :" << domein /** << getDomein() **/ ;
     }
 
-    /*
-    QString xx;
-    */
+    QString contactpersoon_voornaam, contactpersoon_familienaam, doelgroep, domein;
+    bool vrijwilligersverzekering;
 };
 
 #endif // SDEELNEMERORGANISATIE_H
